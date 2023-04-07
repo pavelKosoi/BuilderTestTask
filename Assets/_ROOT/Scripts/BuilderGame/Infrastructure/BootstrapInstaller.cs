a2cd167f79ab7c89d67bea2f31adc6ad86af93f9
andrii.baranchuk <andreybaranchuk98@gmail.com>
2023-04-07 14:34:10 +0300

    Changed targetFrameRate. Added runInBackground.

--- Assets/_ROOT/Scripts/BuilderGame/Infrastructure/BootstrapInstaller.cs
+++ Assets/_ROOT/Scripts/BuilderGame/Infrastructure/BootstrapInstaller.cs
@@ -1,18 +1,27 @@
 using BuilderGame.Infrastructure.Ads;
 using BuilderGame.Infrastructure.Ads.Fake;
 using BuilderGame.Infrastructure.Input;
+using UnityEngine;
 using Zenject;
 
 namespace BuilderGame.Infrastructure
 {
-    public class BootstrapInstaller : MonoInstaller
+    public class BootstrapInstaller : MonoInstaller, IInitializable
     {
         public override void InstallBindings()
         {
+            Container.BindInterfacesTo<BootstrapInstaller>().FromInstance(this);
+            
             Container.Bind<IInputProvider>().To<InputProvider>().AsSingle();
 
             Container.Bind<FakeAdsSettings>().FromResources(nameof(FakeAdsSettings)).AsSingle();
             Container.Bind<IAdvertiser>().To<FakeAdvertiser>().AsSingle();
+        }
+
+        public void Initialize()
+        {
+            Application.runInBackground = true;
+            Application.targetFrameRate = 60;
         }
     }
 }
